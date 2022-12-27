cdc5129fd36bf7e9b902c10d18511f041abfcb48
makeitbad <dmnagy22@gmail.com>
2022-12-27 16:14:07 +0100

    Add files via upload

---
+++ omegga.main.js
@@ -0,0 +1,19 @@
+
+var players = [getPlayers];
+console.log("GAME STARTS")
+while (players < 2) {
+    broadcast("Waiting for another player!")
+}
+// Declare an array of values
+
+
+// Pick a random index from the array
+const index = Math.floor(Math.random() * players.length);
+
+// Get the value at the chosen index
+const bombholder = values[index];
+
+console.log(bombholder); // Outputs a random value from the array
+
+    // Get the selected option
+end
