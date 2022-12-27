97128f7ac0d2df9546005e6afabd779bbb157d9a
makeitbad <dmnagy22@gmail.com>
2022-12-27 17:49:54 +0100

    Update omegga.main.js

--- omegga.main.js
+++ omegga.main.js
@@ -1,19 +1,19 @@
-console.log("GAME STARTS")
-var players = this.omegga.GetPlayers();
-console.log("GAME STARTS")
-while (players < 2) {
-    this.omegga.broadcast("Waiting for another player!")
+const players = this.omegga.getPlayers();
+console.log("plugin ok")
+while (players.length < 2) {
+    broadcast("Waiting for another player!")
 }
 // Declare an array of values
 
 
 // Pick a random index from the array
-const index = Math.floor(Math.random() * players.length);
-
-// Get the value at the chosen index
-const bombholder = values[index];
-
-console.log(bombholder); // Outputs a random value from the array
-
-    // Get the selected option
+if (players.length > 0){const index = Math.floor(Math.random() * players.length);
+   console.log("plugin ok")
+    // Get the value at the chosen index
+    const bombholder = values[index];
+    
+    console.log(bombholder); // Outputs a random value from the array
+    
+        // Get the selected option}
+}s
 end
